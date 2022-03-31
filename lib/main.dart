abe1a7f255c913d12f9a6a5d1edc95c7de15633e
AleLevi <ale.levi@hotmail.com>
2022-03-31 17:55:39 -0300

    commit_adjustment_snapshot.data

--- lib/main.dart
+++ lib/main.dart
@@ -7,11 +7,11 @@ import 'package:async/async.dart';
 import 'dart:convert';
 
 const request =
-    "https://api.hgbrasil.com/finance?format=json-cors&key=de791ef7W";
+    "https://api.hgbrasil.com/finance?format=json-cors&key=de791ef7";
 
 Future<Map> getData() async {
   http.Response response = await http.get(request);
-  json.decode(response.body);
+  return json.decode(response.body);
 }
 
 void main() async {
@@ -34,7 +34,6 @@ class _HomeState extends State<Home> {
   TextEditingController dolarController = TextEditingController();
   TextEditingController euroController = TextEditingController();
 
-  double real;
   double dolar;
   double euro;
 
@@ -49,19 +48,22 @@ class _HomeState extends State<Home> {
       ),
       body: FutureBuilder<Map>(
         future: getData(),
-        builder: (context, snapShot) {
-          switch (snapShot.connectionState) {
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
             case ConnectionState.none:
             case ConnectionState.waiting:
               return Center(
                 child: Text(
                   "Carregando dados.",
-                  style: TextStyle(color: Colors.amber, fontSize: 25.0),
+                  style: TextStyle(
+                    color: Colors.blue,
+                    fontSize: 25.0,
+                  ),
                   textAlign: TextAlign.center,
                 ),
               );
             default:
-              if (snapShot.hasError) {
+              if (snapshot.hasError) {
                 return Center(
                   child: Text(
                     "Erro ao Carregar Dadors :(.",
@@ -70,9 +72,85 @@ class _HomeState extends State<Home> {
                   ),
                 );
               } else {
-                dolar = ;
-                return Container(
-                  color: Colors.green,
+                dolar = snapshot.data["results"]["currencies"]["EUR"]["buy"];
+                euro = snapshot.data["results"]["currencies"]["EUR"]["buy"];
+
+                return SingleChildScrollView(
+                  padding: EdgeInsets.fromLTRB(
+                      10, 0, 10, 0), //Deixa um espaço da borda da tela
+                  child: Form(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment
+                          .stretch, //Estica a coluna para pegar o maximo de tamanho possivel
+                      children: [
+                        Icon(
+                          Icons.monetization_on,
+                          size: 150.0,
+                          color: Color(0xff0149FF),
+                        ),
+                        TextField(
+                          controller: realController,
+                          keyboardType: TextInputType.number,
+                          textAlign: TextAlign.left,
+                          decoration: InputDecoration(
+                            prefix: Text('R\$'),
+                            labelText: "Real",
+                            labelStyle: TextStyle(
+                              color: Color(0xff0149FF),
+                              fontSize: 25.0,
+                            ),
+                            border: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                color: Color(0xff0149FF),
+                              ),
+                            ),
+                          ),
+                          style: TextStyle(
+                            color: Colors.blue,
+                            fontSize: 25.0,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 15,
+                        ),
+                        TextField(
+                          controller: dolarController,
+                          keyboardType: TextInputType.number,
+                          decoration: InputDecoration(
+                            prefix: Text('US\$'),
+                            labelText: "Dólar",
+                            labelStyle: TextStyle(
+                                color: Color(0xff0149FF), fontSize: 25.0),
+                            border: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                color: Color(0xff0149FF),
+                              ),
+                            ),
+                          ),
+                          textAlign: TextAlign.left,
+                        ),
+                        SizedBox(
+                          height: 15,
+                        ),
+                        TextField(
+                          controller: euroController,
+                          keyboardType: TextInputType.number,
+                          decoration: InputDecoration(
+                            prefix: Text('€'),
+                            labelText: "Euro",
+                            labelStyle: TextStyle(
+                                color: Color(0xff0149FF), fontSize: 25.0),
+                            border: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                color: Color(0xff0149FF),
+                              ),
+                            ),
+                          ),
+                          textAlign: TextAlign.left,
+                        ),
+                      ],
+                    ),
+                  ),
                 );
               }
           }
